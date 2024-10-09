3f1251ff77d422f84600bea9b67f0b345b98e26f
JBEstevan <jbe.estevan@gmail.com>
2024-10-09 20:44:15 -0300

    feat: add app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import userRouter from './routers/userRouter.js';
-import booksRouter from './routers/booksRouter.js';
-import rentRouter from './routers/rentRouter.js';
 import dbConfig from './config/dbConfig.js';
 
 dbConfig();
@@ -10,7 +8,5 @@ const app = express();
 app.use(express.json());
 
 app.use('/users', userRouter);
-app.use('/books', booksRouter);
-app.use('/rents', rentRouter);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
